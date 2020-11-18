2b156520d432e9a27769345f0b21a558cf3055d0
Sebas <juarezsebastian297@gmail.com>
2020-11-18 15:19:55 -0600

    Add js & update the html

---
+++ main.js
@@ -0,0 +1,19 @@
+let addToDo = document.getElementById("addToDo");
+let toDosContainer = document.getElementById('toDosContainer');
+let inputField = document.getElementById('inputField');
+
+addToDo.addEventListener('click',function () {
+  var paragraph=document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText=inputField.value;
+  toDosContainer.appendChild(paragraph);
+  inputField.value="";
+
+  paragraph.addEventListener('click',function(){
+    paragraph.style.textDecoration="line-through";
+  })
+
+  paragraph.addEventListener('dblclick',function(){
+    toDosContainer.removeChild(paragraph);
+  })
+})
